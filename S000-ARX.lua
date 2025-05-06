5e10f98097c0969a10aea0874cbb6302f4724953
AljoshPorteza <aljoshporteza@gmail.com>
2025-05-06 08:47:10 -0400

    added 10 second wait before loading script

--- S000-ARX.lua
+++ S000-ARX.lua
@@ -1,3 +1,4 @@
+wait(10)
 local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
