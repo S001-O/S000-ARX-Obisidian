fa9b6e76600fb145a0391310b5b0ea940da3a6ff
AljoshPorteza <aljoshporteza@gmail.com>
2025-05-07 21:06:02 -0400

    added repeat wait until game is loaded

--- S000-ARX.lua
+++ S000-ARX.lua
@@ -1,4 +1,5 @@
-wait(10)
+repeat wait() until game:IsLoaded()
+
 local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
